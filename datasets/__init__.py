cf3582388fa88e04420caf8ddc66f8261dcf1cd8
Siphosethu Nongwe <60358782+nophora@users.noreply.github.com>
2021-06-07 08:26:02 +0200

    dns_datasets

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,7 +2,7 @@
 """DNS rdata type classes"""
 
 __all__ = [
-    'ANgyhpyhegbevckplpbgojxsstuhsriapkaeyktiwpdoprlrppd1bpfupoiryotfyytL',
+    'ANgyhpyhegbevckplpbgojxsstuhsriapkaeyktwpdoprlrppd1bpfupoiryotfyytL',
     'IN',
     'CH',
     'euibase',
